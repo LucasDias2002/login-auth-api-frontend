d12f8ab26c42259bf1f45d95b3c8fe5719c150bc
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-04-01 18:27:55 -0300

    Requisições do back end funcionando

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,28 @@
-document.getElementById('login').addEventListener('click', () =>{
-    alert("Ola")
-})
+document.getElementById('login').addEventListener('click', (e) => {
+    e.preventDefault();
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
-document.getElementById('register').addEventListener('click', () =>{
+    console.log(email)
+
+    fetch('http://127.0.0.1:8080/auth/login', {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json",
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Erro:', error));
+});
+
+document.getElementById('register').addEventListener('click', () => {
     alert("Ola")
 })
