0255d4e6f50cc56c2c3732a8df6f8f8385782041
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-04-03 21:17:23 -0300

    Alterações na rota padrão

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,88 @@
-document.getElementById('login').addEventListener('click', (e) => {
-    e.preventDefault();
+async function login() {
+
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
-    console.log(email)
+    let box = document.getElementById('messageLogin');
+    box.innerHTML = "";
 
-    fetch('http://127.0.0.1:8080/auth/login', {
-        method: 'POST',
-        headers: {
-            "Accept": "application/json",
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => console.error('Erro:', error));
-});
+    try {
+        console.log("Enviando requisição de login...");
 
-document.getElementById('register').addEventListener('click', () => {
-    alert("Ola")
-})
+        let response = await fetch('http://127.0.0.1:8080/auth/login', {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+
+        let data = await response.json();
+        console.log("Token gerado com sucesso: \n")
+
+        box.innerHTML = await addMessage("Login feito com sucesso!", "greenBox", "check");
+
+    } catch (error) {
+        box.innerHTML = await addMessage("Erro ao efetuar login!", "redBox", "x");
+
+        console.error("Erro:", error);
+    }
+
+}
+
+async function register() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let box = document.getElementById('messageRegister');
+    box.innerHTML = "";
+
+    try {
+        console.log("Enviando requisição de registro...");
+
+        let response = await fetch('http://127.0.0.1:8080/auth/register', {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+
+        let data = await response.json();
+        console.log("Token gerado com sucesso: \n")
+
+        box.innerHTML = await addMessage("Cadastrado com sucesso!", "greenBox", "check");
+
+    } catch (error) {
+        box.innerHTML = await addMessage("Erro ao efetuar cadastrar!", "redBox", "x");
+
+        console.error("Erro:", error);
+    }
+}
+
+async function addMessage(message, color, icon) {
+    return `<div id="message" class="message ${color}">
+            <div class="res">
+                <i class="bi bi-${icon}"></i>
+                <p>${message}</p>
+            </div>
+        </div>`
+}
